d3b20b7a19b67c83b45e72762db00d9576a289a3
KRISHNA RAJ A <93074788+krishnaraj2002@users.noreply.github.com>
2023-10-29 13:45:26 +0530

    task 3

---
+++ calculator/script.js
@@ -0,0 +1,22 @@
+let display = document.getElementById("display");
+let currentInput = "";
+
+function appendToDisplay(value) {
+    currentInput += value;
+    display.textContent = currentInput;
+}
+
+function clearDisplay() {
+    currentInput = "";
+    display.textContent = "0";
+}
+
+function calculateResult() {
+    try {
+        currentInput = eval(currentInput);
+        display.textContent = currentInput;
+    } catch (error) {
+        display.textContent = "Error";
+        currentInput = "";
+    }
+}
